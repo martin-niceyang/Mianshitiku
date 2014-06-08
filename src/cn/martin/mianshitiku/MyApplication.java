014eefb6c51297cf3aba235d89895879ab765fd6
martin <martin.niceyang@gmail.com>
2014-06-08 17:37:55 +0800

    add mainActivity and layout

---
+++ src/cn/martin/mianshitiku/MyApplication.java
@@ -0,0 +1,7 @@
+package cn.martin.mianshitiku;
+
+import android.app.Application;
+
+public class MyApplication extends Application {
+
+}
---
+++ src/cn/martin/mianshitiku/activity/MainActivity.java
@@ -0,0 +1,16 @@
+package cn.martin.mianshitiku.activity;
+
+import cn.martin.mianshitiku.R;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity  extends Activity{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		
+	}
+	
+}
